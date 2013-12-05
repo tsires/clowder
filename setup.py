0283d30938e12f1404829924788d94f573d1f3ad
Tom Sires <tsires@uno.edu>
2013-12-05 00:10:50 -0600

    Update version to 0.2.0

--- setup.py
+++ setup.py
@@ -2,21 +2,22 @@ from setuptools import setup
 
 setup(
     name='clowder',
-    version='0.1.7',
+    version='0.2.0',
     author='Nick Kiermaier, Chad Ross, Tom Sires',
-    author_email='nickthemagicman@live.com',
+    author_email='tsires@uno.edu',
     packages=['clowder'],
     scripts=[],
-    url='https://gitlab.cs.uno.edu/tsires/distfs',
+    url='https://gitlab.cs.uno.edu/tsires/clowder',
     license='LICENSE.txt',
     description='ClowderFS: Distributed Filesystem project for CSCI 6450',
     long_description=open('README').read(),
     install_requires=[
-        "pyzmq",
         "kazoo",
         "msgpack-python",
         "fusepy",
         "mmh3",
+	"flask",
+	"requests",
     ],
     dependency_links=[
         "git+https://github.com/tsires/fusepy.git#fusepy",
@@ -25,18 +26,7 @@ setup(
         'console_scripts':[
 		'mkfs.clowder = clowder.cli:mkfs',
 		'mount.clowder = clowder.cli:mount',
+		'chunkserver.clowder = clowder.cli:chunk',
 		]
         }
 )
-
-
-"""
-check the setup.py for validity-> python setup.py check
-
-
-
-pip list ->lists all pip packages
-python setup.py sdist -> generate distribution package
-sudo pip install <package>
-sudo pip uninstall <package>
-"""
